12de53ff9387f07d8505667b0bad3dabe0f17f1b
AlexScotte <scottealexandre@gmail.com>
2023-02-07 08:32:22 +0100

    [Project #1] Improve variables management and add method to add proposal and add vote

--- contracts/voting-system/voting.sol
+++ contracts/voting-system/voting.sol
@@ -32,20 +32,24 @@ contract voting is Ownable{
         uint voteCount;
     }
 
-    mapping(address => bool) voterWhitelist;
+    mapping(address => Voter) voters;
     uint private _voterWhitelistedCount = 0;
 
-    mapping(address => Proposal) voterProposals;
+    mapping(address => uint) voterProposalIds;
+    Proposal[] voterProposals;
     
+    WorkflowStatus private _currentVotingSession;
+
     function adminWhitelist(address _addrVoter) external onlyOwner{
 
         // TODO: can whitelist the admin ? 
 
 
-        require(!voterWhitelist[_addrVoter], "The voter is already whitelisted.");
-
-        voterWhitelist[_addrVoter] = true;
+        require(!voters[_addrVoter].isRegistered, "The voter is already whitelisted.");
+        
+        voters[_addrVoter] = Voter(true, false, 0);
         _voterWhitelistedCount++;
+
         emit VoterRegistered(_addrVoter);
     }
 
@@ -53,23 +57,61 @@ contract voting is Ownable{
 
         require(_voterWhitelistedCount > 1, "Need at least 2 whitelisted voter to start a proposal session.");
 
+        WorkflowStatus previousStatus = _currentVotingSession;
+        _currentVotingSession = WorkflowStatus.ProposalsRegistrationStarted;
 
+        emit WorkflowStatusChange(previousStatus, _currentVotingSession);
+    }
+
+    function adminStopProposalsSession() external onlyOwner{
+
+        // require(_voterWhitelistedCount > 1, "Need at least 2 whitelisted voter to start a proposal session.");
+        require(_currentVotingSession == WorkflowStatus.ProposalsRegistrationStarted, "The proposal registration is not started yet.");
+
+        WorkflowStatus previousStatus = _currentVotingSession;        
+        _currentVotingSession = WorkflowStatus.ProposalsRegistrationEnded;
+
+        emit WorkflowStatusChange(previousStatus, _currentVotingSession);
+    }
+
+    function adminStartVotingSession() external onlyOwner{
+
+        require(voterProposals.length > 1, "Need at least 2 proposals to start a voting session.");
+
+        WorkflowStatus previousStatus = _currentVotingSession;
+        _currentVotingSession = WorkflowStatus.VotingSessionStarted;
+
+        emit WorkflowStatusChange(previousStatus, _currentVotingSession);
     }
 
 
 
-    function voterProposal(string calldata _description) external {
+
+    function voterAddProposal(string calldata _description) external {
         
-        require(voterWhitelist[msg.sender], "The voter must be whitelisted to make a proposal.");
+        require(_currentVotingSession == WorkflowStatus.ProposalsRegistrationStarted, "The proposal registration is not started yet.");
+        require(voters[msg.sender].isRegistered, "You must be whitelisted to make a proposal.");
+        require(bytes(_description).length > 0, "The proposal cannot be empty.");
 
         // One proposal only ? if yes add require
 
-        voterProposals[msg.sender] = Proposal(_description, 0);
+        voterProposals.push(Proposal(_description, 0));
+        uint proposalId = voterProposals.length - 1;
+        voterProposalIds[msg.sender] = proposalId;
 
-        // TODO:
-        // emit ProposalRegistered();
+        emit ProposalRegistered(proposalId);
     }
 
+    function voterAddVote(uint _proposalId) external{
+
+        require(_currentVotingSession == WorkflowStatus.VotingSessionStarted, "The voting session is not started yet.");
+        require(voters[msg.sender].isRegistered, "You must be whitelisted to vote.");
+        require(voters[msg.sender].hasVoted, "You already voted.");
+        require(bytes(voterProposals[_proposalId].description).length > 0, "This proposal does not exists.");
+        
+        voters[msg.sender].votedProposalId = _proposalId;
+        voters[msg.sender].hasVoted = true;
+    }
 
 
 
