5229a1b21a410609b69d3161b9f0c66118922f0f
AlexScotte <scottealexandre@gmail.com>
2023-02-09 21:49:22 +0100

    update exo cours

--- contracts/exoCours/exoBalance.sol
+++ contracts/exoCours/exoBalance.sol
@@ -29,9 +29,28 @@ contract exoBalance {
     
     function transferBalanceCheck(uint amount) public payable returns(bool){
 
+        // Send and transfert cost 24 000 gas
+        // pure transaction cost already 21000 gas 
+        // it remains not enough gas to add data in the transaction
+
+        // Call will use all the necessary gas to do the transaction
+        // need to put a gas limit 
+        // it's more flexible than send an transfer
+        // no revert, returns a bool and data  
+
         require(msg.value > 1, "You need to send more money.");
         require(myAdress.balance > amount, "Not enough money to send.");
         (bool success,) = myAdress.call{value: msg.value}("");
         return success;
+    }
+
+    receive() external payable {
+        // Execute only when the contract receive ether
+        // If ether + data are sent, it's the fallback method that is executed
+
+    }
+
+    fallback() external payable {
+        // Executed when ether + data are sent or if there is no receive method
     }
 }
